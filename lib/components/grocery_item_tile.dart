58e14794ea3fc7ff89111ecb050f3685bb471b88
aaronhayu <aaronhayu@gmail.com>
2025-03-09 21:52:51 +0100

    Added cart page and shopping bag

--- lib/components/grocery_item_tile.dart
+++ lib/components/grocery_item_tile.dart
@@ -16,14 +16,42 @@ class GroceryItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(children: [
-        // image
-        Image.asset(imagePath),
-        // item name
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+            color: color[100],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+          // image
+          Image.asset(
+            imagePath,
+            height: 64,
+          ),
 
-        // price + buttom
-      ]),
+
+          // item name
+          Text(itemName),
+
+          // price + button
+          MaterialButton(
+            onPressed: () {},
+            color: color[800],
+            child: Text(
+              '\$' + itemPrice,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+
+        ]),
+      ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_shop/model/cart_model.dart';
+import 'package:grocery_shop/pages/cart_page.dart';
 import 'package:provider/provider.dart';
 
 import '../components/grocery_item_tile.dart';
@@ -10,6 +11,18 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.push(
+              context, 
+              MaterialPageRoute(
+                  builder: (context) {
+                    return CartPage();
+                  },
+              ),
+          ),
+        backgroundColor: Colors.black,
+        child: Icon(Icons.shopping_bag)
+      ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -63,8 +76,13 @@ class HomePage extends StatelessWidget {
                 builder: (context, value, child) {
                   return GridView.builder(
                     itemCount: value.shopItems.length,
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2),
+                    padding: EdgeInsets.all(12),
+                    gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 1/1.3,
+                        ),
+
                     itemBuilder: (context, index) {
                       return GroceryItemTile(
                         itemName: value.shopItems[index][0],
