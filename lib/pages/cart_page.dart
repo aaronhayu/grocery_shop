b61ae6300134fbd19c606d067a81890b488d7abd
aaronhayu <aaronhayu@gmail.com>
2025-03-12 22:43:22 +0100

    Add Total Price and Pay now button and remove items from cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,12 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../model/cart_model.dart';
 
 class CartPage extends StatelessWidget {
-  const CartPage ({super.key});
+  const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("My Cart"),),
+      appBar: AppBar(title: Text('My Cart'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Consumer<CartModel>(
+        builder: (context, value, child) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // List of cart item.
+              Expanded(
+                child: ListView.builder(
+                  itemCount: value.cartItems.length,
+                  padding: EdgeInsets.all(12),
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: ListTile(
+                          leading: Image.asset(
+                            value.cartItems[index][2],
+                            height: 36,
+                          ),
+                          title: Text(value.cartItems[index][0]),
+                          subtitle: Text('\$' + value.cartItems[index][1]),
+                          trailing: IconButton(
+                              icon: Icon(Icons.cancel),
+                              onPressed: () =>
+                                Provider.of<CartModel>(context, listen: false)
+                                .removeItemFromCart(index),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+
+              // Total + pay now.
+              Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: EdgeInsets.all(24),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Price
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Total Price: ",
+                            style: TextStyle(color: Colors.green[100]),
+                          ),
+                          const SizedBox(height: 4,),
+                          Text(
+                            '\$' + value.calculateTotal(),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      // Pay now button
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.green.shade100),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: EdgeInsets.all(12),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Pay Now",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 16,
+                                color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          );
+        },
+      ),
     );
   }
 }
